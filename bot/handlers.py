54b23f93abdc6b873b4c519da82ab2d15565fdd9
AndrewDya <andrewdya@gmail.com>
2023-07-20 16:01:06 +0600

    func popular add 1,3,5,10 positions

--- bot/handlers.py
+++ bot/handlers.py
@@ -21,21 +21,25 @@ async def handle_button_press(update: Update, context: ContextTypes.DEFAULT_TYPE
         await search_command(update, context)
     elif button_data == "history_command":
         await history_command(update, context)
+    elif button_data.startswith("popular_"):  # Обработка выбора числа популярных фильмов
+        await on_popular_count_selected(update, context)
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Неизвестная команда")
 
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # Создание кнопок команд
+    # Создание кнопок команд с визуальными стилями
     buttons_row1 = [
-        InlineKeyboardButton("Помощь", callback_data="help_command"),
-        InlineKeyboardButton("Популярные фильмы", callback_data="popular_command"),
+        InlineKeyboardButton("Помощь 🤔", callback_data="help_command"),
+        InlineKeyboardButton("Популярные фильмы 🎬", callback_data="popular_command"),
     ]
     buttons_row2 = [
-        InlineKeyboardButton("Топ рейтинга", callback_data="top_rated_command"),
-        InlineKeyboardButton("Ожидаемые", callback_data="upcoming_command"),
+        InlineKeyboardButton("Топ рейтинга 👍", callback_data="top_rated_command"),
+        InlineKeyboardButton("Ожидаемые 🚀", callback_data="upcoming_command"),
     ]
     buttons_row3 = [
-        InlineKeyboardButton("Поиск фильма", callback_data="search_command"),
-        InlineKeyboardButton("История", callback_data="history_command"),
+        InlineKeyboardButton("Поиск фильма 🔍", callback_data="search_command"),
+        InlineKeyboardButton("История 📚", callback_data="history_command"),
     ]
 
     # Создание разметки с кнопками
@@ -49,22 +53,49 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response = "Доступные команды:\n"
-    response += "/help - Получить помощь по командам бота\n"
-    response += "/popular - Получить список популярных фильмов\n"
-    response += "/top_rated - Получить список фильмов с высоким рейтингом\n"
-    response += "/upcoming - Получить список ожидаемых фильмов\n"
-    response += "/search <название фильма> - Поиск фильма по названию\n"
-    response += "/history - Просмотреть историю последних запросов\n"
+    response += "Помощь - Информация по командам бота\n"
+    response += "Популярные фильмы - Получить список популярных фильмов\n"
+    response += "Топ рейтинга - Получить список фильмов с высоким рейтингом\n"
+    response += "Ожидаемые - Получить список ожидаемых фильмов\n"
+    response += "Поиск фильма - После этой команды введите название фильма " \
+                "и бот предоставит результаты поиска\n"
+    response += "История - Просмотреть историю последних запросов\n"
 
+    # Отправка сообщения с текстом команд и кнопками
     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
+
+    # Вызов функции start_command для отображения кнопок
+    await start_command(update, context)
 
 
 async def popular_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Варианты числа фильмов для вывода в кнопках
+    movie_counts = [1, 3, 5, 10]
+
+    # Создание кнопок с вариантами числа фильмов
+    buttons = [
+        InlineKeyboardButton(str(count), callback_data=f"popular_{count}")
+        for count in movie_counts
+    ]
+    keyboard = InlineKeyboardMarkup([buttons])
+
+    # Отправка сообщения с вопросом о выборе числа фильмов
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Выберите количество популярных фильмов, которое хотите увидеть:",
+        reply_markup=keyboard
+    )
+
+
+async def on_popular_count_selected(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Получение выбранного числа фильмов из CallbackQuery
+    selected_count = int(update.callback_query.data.split("_")[1])
+
     url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&page=1&language={language}"
 
     data = await send_http_request(url)
     if data and "results" in data:
-        movies = data["results"][:5]  # TODO: Изменить 5, добавить ввод от пользователя
+        movies = data["results"][:selected_count]
 
         for movie in movies:
             poster_url, movie_info, keyboard = await view_movie_info(movie)
