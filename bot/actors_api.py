feeab2f43072d86388baab7f07a80d4e41988ab6
AndrewDya <andrewdya@gmail.com>
2023-07-30 11:05:40 +0600

    modifed actors info

--- bot/actors_api.py
+++ bot/actors_api.py
@@ -1,6 +1,6 @@
 import io
 from bot.utils import send_http_request, API_KEY, load_photo_content, \
-    format_date, process_overview
+    format_date, process_overview, language
 
 
 async def get_actor_details(actor_id):
@@ -22,13 +22,33 @@ async def get_actor_details(actor_id):
     return None
 
 
+async def get_movies_list(actor_id):
+    actor_name = await get_name_from_id(actor_id)
+    if not actor_name:
+        return None
+
+    url = f"https://api.themoviedb.org/3/search/person?api_key={API_KEY}&query={actor_name}&language={language}"
+    data = await send_http_request(url)
+
+    if data and data["results"]:
+        actor_info = data["results"][0]
+        known_for = actor_info.get("known_for", [])
+        movie_names = [movie.get("title") for movie in known_for]
+
+        return movie_names
+
+    return None
+
+
 async def view_actor_info(actor):
     actor_id = actor.get("id")
     actor_name, photo_url, formatted_birthday, overview = await get_actor_details(actor_id)
+    movie_names = await get_movies_list(actor_id)
 
     actor_info = f"{actor_name}\n"
     actor_info += f"Дата рождения: {formatted_birthday}\n"
     actor_info += f"Биография: {overview}\n"
+    actor_info += f"Фильмография: {', '.join(movie for movie in movie_names if movie)}\n"
 
     return photo_url, actor_info
 
@@ -48,3 +68,10 @@ async def send_actors_info(update, context, actors):
                 chat_id=update.effective_chat.id, text=actor_info
             )
 
+
+async def get_name_from_id(actor_id):
+    url = f"https://api.themoviedb.org/3/person/{actor_id}?api_key={API_KEY}"
+    data = await send_http_request(url)
+    if data:
+        name = data.get("name")
+        return name
